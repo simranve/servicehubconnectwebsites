7a22784d2fec0126834847a38f27afb1b378381b
Mahipal <simrangupta@virtualemployee.com>
2022-01-11 17:00:03 +0530

    changes

--- src/container/Homepage/TrackBusinessUsers.js
+++ src/container/Homepage/TrackBusinessUsers.js
@@ -6,7 +6,7 @@ import Footer from "../Footer";
 import * as actions from "../../store/actions/index";
 import { connect } from "react-redux";
 import { withSnackbar } from "notistack";
-
+import { MDBDataTable } from "mdbreact";
 import rating from "../../assets/images/rating_on_ic.png";
 // export default function TrackBusinessUsers() {
 class TrackBusinessUsers extends React.Component {
@@ -18,20 +18,19 @@ class TrackBusinessUsers extends React.Component {
   }
   componentDidMount() {
     
-    let data = {}
-    let self = this
+    let data = {};
+    let self = this;
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log("timezone",timezone);
     if (self.props != undefined && self.props.location != undefined && self.props.location.aboutProps != undefined && self.props.location.aboutProps.name != undefined) {
       var name = self.props.location.aboutProps.name
       navigator.geolocation.getCurrentPosition(function(position) {
-        // console.log("Latitude is :", position.coords.latitude);
-        // console.log("Longitude is :", position.coords.longitude);
         data = {
-          subCategoryId:name,
+          subCategoryId:name,//"6055b92178d8eb46148fb5a9",
           // latitude:position.coords.latitude,
           // longitude:position.coords.longitude,
           longitude:-88.27857348227539,
         	latitude:40.08201745156272,
-          day:(new Date()).getDay()
         };
         self.props.fetchbussinessList(data)  
       });
@@ -39,7 +38,117 @@ class TrackBusinessUsers extends React.Component {
     }
   }
   render() {
-    const Data = this.props.bussinesslisting;
+    let businessCategoryTable = <div />;
+    let categoryList = [];
+    let stars = <div></div>
+    {this.props.bussinesslisting.length > 0
+      ? this.props.bussinesslisting.map(e => {
+        if(Math.floor(e.businessRating) == 0){
+          stars = <div> No Stars</div>
+        }
+        else if(Math.floor(e.businessRating) == 1){
+          stars = <div>
+            <img src={rating}/>{e.businessRating} Star Rating
+          </div>
+        }
+        
+        else if(Math.floor(e.businessRating) == 2){
+          stars = <div>
+            <img src={rating}/>
+            <img src={rating}/>{e.businessRating} Star Rating
+          </div>
+        }
+        
+        else if(Math.floor(e.businessRating) == 3){
+          stars = <div>
+            <img src={rating}/>
+            <img src={rating}/>
+            <img src={rating}/>{e.businessRating} Star Rating
+          </div>
+        }
+        
+        else if(Math.floor(e.businessRating) == 4){
+          stars = <div>
+            <img src={rating}/>
+            <img src={rating}/>
+            <img src={rating}/>
+            <img src={rating}/>{e.businessRating} Star Rating
+          </div>
+        }
+
+        else if(Math.floor(e.businessRating) == 5){
+          stars = <div>
+            <img src={rating}/>
+            <img src={rating}/>
+            <img src={rating}/>
+            <img src={rating}/>
+            <img src={rating}/>{e.businessRating} Star Rating
+          </div>
+        }
+        categoryList.push(
+          {
+            business_logo:<a target="_blank" href={e.website}><div className="carpoolImage"><img src={e.business_logo} /></div></a>,
+            data:<a target="_blank" href={e.website}>
+              <div className="carpoolContent" style={{ borderRight: "1px solid #999", flex: 1 }} >
+                <div className="carpoolImgCtnt">
+                  <div>
+                    <p>Happy Hours</p>
+                    <span>{e.bussiness_info.happy_hours.start_time}, {e.bussiness_info.happy_hours.end_time}</span>
+                  </div>
+                  <div className="ratingBox">{e.bussiness_info.happy_hours.discount}%</div>
+                </div>
+                <h3>{e.bussiness_name}</h3>
+                <p>{e.business_owner_name}</p>
+                <p>{e.local_city}</p>
+                <p>{e.bussiness_email}</p>
+                <p>Contact Number {e.country_code} {e.mobile_no}</p>
+                <p>
+                  
+                  <div>{stars}</div>
+                </p>
+                <p>
+                  <span>
+                      {
+                        ((new Date()).getDay() == 1)?(e.business_weekdays.is_monday_Open?"Opened":"Closed"):
+                        ((new Date()).getDay() == 2)?(e.business_weekdays.is_tuesday_Open?"Opened":"Closed"):
+                        ((new Date()).getDay() == 3)?(e.business_weekdays.is_wednesday_Open?"Opened":"Closed"):
+                        ((new Date()).getDay() == 4)?(e.business_weekdays.is_thursday_Open?"Opened":"Closed"):
+                        ((new Date()).getDay() == 5)?(e.business_weekdays.is_friday_Open?"Opened":"Closed"):
+                        ((new Date()).getDay() == 6)?(e.business_weekdays.is_saturday_Open?"Opened":"Closed"):
+                        (e.business_weekdays.is_sunday_Open?"Opened":"Closed")
+
+                      }
+                  </span>
+                </p>
+
+              </div>
+            </a>
+            }
+        )
+      })
+
+      : categoryList.push(
+        ['No record']
+      )}
+      const data = {
+        columns: [
+          {
+            label: "Business Logo",
+            field: "business_logo",
+            sort: "asc",
+          },
+          {
+            label: "Data",
+            field: "data",
+            sort: "asc",
+          },
+        ],
+        rows: categoryList,
+      };
+  
+      businessCategoryTable = (
+        <MDBDataTable striped bordered hover entries={10} data={data} />
+      );
     return (
       <>
         <Header />
@@ -49,40 +158,9 @@ class TrackBusinessUsers extends React.Component {
             <div className="d-flex mb-3">
               {/* <h3 className="uprheadng">Dashboard / Ride Share / Location / </h3>
               <h3 className="txtBlk"> Carpool</h3> */}
-              <h3 className="txtBlk">Professional Listing</h3>
+              <h3 className="txtBlk">Business Listing</h3>
             </div>
-
-            {Data.length > 0 ? Data.map((e) => (
-              <a target="_blank" href={e.website}>
-                <div className="shadowCont mt-12">
-                  <div className="row px-12">
-                    <div className="col-sm-12 d-flex align-items-center">
-                      <div className="carpoolImage">
-                        <img src={e.business_logo} />
-                      </div>
-                        <div
-                            className="carpoolContent"
-                            style={{ borderRight: "1px solid #999", flex: 1 }}
-                        >
-                            <h3>{e.bussiness_name}</h3>
-                            <p>{e.business_owner_name}</p>
-                            <p>{e.local_city}</p>
-                            <p>{e.bussiness_email}</p>
-                            <p>Contact Number {e.country_code} {e.mobile_no}</p>
-                            <p>
-                              <img
-                                src={rating}
-                              />{" "}
-                              <span>{e.businessRating} Star Rating</span>
-                            </p>
-                            
-
-                        </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            )):<div>No Data Found</div>}
+              {businessCategoryTable}
           </div>
         </section>
 
