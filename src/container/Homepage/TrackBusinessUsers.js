d5720f2ec99b3190211725f1d5eeb034130052f5
Mahipal <simrangupta@virtualemployee.com>
2022-01-11 18:28:41 +0530

    changes

--- src/container/Homepage/TrackBusinessUsers.js
+++ src/container/Homepage/TrackBusinessUsers.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
-
 import * as actions from "../../store/actions/index";
 import { connect } from "react-redux";
 import { withSnackbar } from "notistack";
@@ -11,7 +10,6 @@ import rating from "../../assets/images/rating_on_ic.png";
 // export default function TrackBusinessUsers() {
 class TrackBusinessUsers extends React.Component {
   constructor(props) {
-    console.log(props)
     super(props)
     this.state = {
     }
@@ -27,10 +25,11 @@ class TrackBusinessUsers extends React.Component {
       navigator.geolocation.getCurrentPosition(function(position) {
         data = {
           subCategoryId:name,//"6055b92178d8eb46148fb5a9",
-          // latitude:position.coords.latitude,
-          // longitude:position.coords.longitude,
-          longitude:-88.27857348227539,
-        	latitude:40.08201745156272,
+          latitude:position.coords.latitude,
+          longitude:position.coords.longitude,
+          // longitude:-88.27857348227539,
+        	// latitude:40.08201745156272,
+          time_zone :timezone
         };
         self.props.fetchbussinessList(data)  
       });
@@ -43,88 +42,95 @@ class TrackBusinessUsers extends React.Component {
     let stars = <div></div>
     {this.props.bussinesslisting.length > 0
       ? this.props.bussinesslisting.map(e => {
-        if(Math.floor(e.businessRating) == 0){
-          stars = <div> No Stars</div>
-        }
-        else if(Math.floor(e.businessRating) == 1){
-          stars = <div>
-            <img src={rating}/>{e.businessRating} Star Rating
-          </div>
-        }
-        
-        else if(Math.floor(e.businessRating) == 2){
-          stars = <div>
-            <img src={rating}/>
-            <img src={rating}/>{e.businessRating} Star Rating
-          </div>
-        }
-        
-        else if(Math.floor(e.businessRating) == 3){
-          stars = <div>
-            <img src={rating}/>
-            <img src={rating}/>
-            <img src={rating}/>{e.businessRating} Star Rating
-          </div>
-        }
-        
-        else if(Math.floor(e.businessRating) == 4){
-          stars = <div>
-            <img src={rating}/>
-            <img src={rating}/>
-            <img src={rating}/>
-            <img src={rating}/>{e.businessRating} Star Rating
-          </div>
-        }
+        if(e.website == ""){
 
-        else if(Math.floor(e.businessRating) == 5){
-          stars = <div>
-            <img src={rating}/>
-            <img src={rating}/>
-            <img src={rating}/>
-            <img src={rating}/>
-            <img src={rating}/>{e.businessRating} Star Rating
-          </div>
         }
-        categoryList.push(
-          {
-            business_logo:<a target="_blank" href={e.website}><div className="carpoolImage"><img src={e.business_logo} /></div></a>,
-            data:<a target="_blank" href={e.website}>
-              <div className="carpoolContent" style={{ borderRight: "1px solid #999", flex: 1 }} >
-                <div className="carpoolImgCtnt">
+        else{
+          if(Math.floor(e.businessRating) == 0){
+            stars = <div> No Stars</div>
+          }
+          else if(Math.floor(e.businessRating) == 1){
+            stars = <div>
+              <img src={rating}/>{e.businessRating} Star Rating
+            </div>
+          }
+          
+          else if(Math.floor(e.businessRating) == 2){
+            stars = <div>
+              <img src={rating}/>
+              <img src={rating}/>{e.businessRating} Star Rating
+            </div>
+          }
+          
+          else if(Math.floor(e.businessRating) == 3){
+            stars = <div>
+              <img src={rating}/>
+              <img src={rating}/>
+              <img src={rating}/>{e.businessRating} Star Rating
+            </div>
+          }
+          
+          else if(Math.floor(e.businessRating) == 4){
+            stars = <div>
+              <img src={rating}/>
+              <img src={rating}/>
+              <img src={rating}/>
+              <img src={rating}/>{e.businessRating} Star Rating
+            </div>
+          }
+  
+          else if(Math.floor(e.businessRating) == 5){
+            stars = <div>
+              <img src={rating}/>
+              <img src={rating}/>
+              <img src={rating}/>
+              <img src={rating}/>
+              <img src={rating}/>{e.businessRating} Star Rating
+            </div>
+          }
+          categoryList.push(
+            {
+              business_logo:<a target="_blank" href={e.website}><div className="carpoolImage"><img src={e.business_logo} /></div></a>,
+              data:<a target="_blank" href={e.website}>
+                <div className="carpoolContent" style={{ borderRight: "1px solid #999", flex: 1 }} >
                   <div>
-                    <p>Happy Hours</p>
-                    <span>{e.bussiness_info.happy_hours.start_time}, {e.bussiness_info.happy_hours.end_time}</span>
+                      <p>Happy Hours</p>
+                      <span>{e.bussiness_info.happy_hours.start_time} - {e.bussiness_info.happy_hours.end_time}</span>
+                      <div className="ratingBox">{e.bussiness_info.happy_hours.discount}%</div>
                   </div>
-                  <div className="ratingBox">{e.bussiness_info.happy_hours.discount}%</div>
+                    
+                  <h3>{e.bussiness_name}</h3>
+                  <p>{e.business_owner_name}</p>
+                  <p>{e.local_city}</p>
+                  <p>{e.bussiness_email}</p>
+                  <p>Contact Number {e.country_code} {e.mobile_no}</p>
+                  <p>
+                    
+                    <div>{stars}</div>
+                  </p>
+                  <p>
+                    <span>
+                        {
+                          (e.business_weekdays != undefined?
+                          ((new Date()).getDay() == 1)?(e.business_weekdays.is_monday_Open?"Opened":"Closed"):
+                          ((new Date()).getDay() == 2)?(e.business_weekdays.is_tuesday_Open?"Opened":"Closed"):
+                          ((new Date()).getDay() == 3)?(e.business_weekdays.is_wednesday_Open?"Opened":"Closed"):
+                          ((new Date()).getDay() == 4)?(e.business_weekdays.is_thursday_Open?"Opened":"Closed"):
+                          ((new Date()).getDay() == 5)?(e.business_weekdays.is_friday_Open?"Opened":"Closed"):
+                          ((new Date()).getDay() == 6)?(e.business_weekdays.is_saturday_Open?"Opened":"Closed"):
+                          (e.business_weekdays.is_sunday_Open?"Opened":"Closed")
+                          :"Closed")
+  
+                        }
+                    </span>
+                  </p>
+  
                 </div>
-                <h3>{e.bussiness_name}</h3>
-                <p>{e.business_owner_name}</p>
-                <p>{e.local_city}</p>
-                <p>{e.bussiness_email}</p>
-                <p>Contact Number {e.country_code} {e.mobile_no}</p>
-                <p>
-                  
-                  <div>{stars}</div>
-                </p>
-                <p>
-                  <span>
-                      {
-                        ((new Date()).getDay() == 1)?(e.business_weekdays.is_monday_Open?"Opened":"Closed"):
-                        ((new Date()).getDay() == 2)?(e.business_weekdays.is_tuesday_Open?"Opened":"Closed"):
-                        ((new Date()).getDay() == 3)?(e.business_weekdays.is_wednesday_Open?"Opened":"Closed"):
-                        ((new Date()).getDay() == 4)?(e.business_weekdays.is_thursday_Open?"Opened":"Closed"):
-                        ((new Date()).getDay() == 5)?(e.business_weekdays.is_friday_Open?"Opened":"Closed"):
-                        ((new Date()).getDay() == 6)?(e.business_weekdays.is_saturday_Open?"Opened":"Closed"):
-                        (e.business_weekdays.is_sunday_Open?"Opened":"Closed")
-
-                      }
-                  </span>
-                </p>
-
-              </div>
-            </a>
-            }
-        )
+              </a>
+              }
+          )
+        }
+        
       })
 
       : categoryList.push(
@@ -160,7 +166,9 @@ class TrackBusinessUsers extends React.Component {
               <h3 className="txtBlk"> Carpool</h3> */}
               <h3 className="txtBlk">Business Listing</h3>
             </div>
+            <div className='table-responsive'>
               {businessCategoryTable}
+            </div>
           </div>
         </section>
 
